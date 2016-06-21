5c17a8db43c764c3164fff2f240b7cd34ce17e54
sidneyli <liteng0219@gmail.com>
2016-06-21 11:29:30 +0800

    first commit

---
+++ LibraryWeb/LibraryWeb/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BitTitan.LibraryWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ LibraryWeb/LibraryWeb/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace BitTitan.LibraryWeb.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
